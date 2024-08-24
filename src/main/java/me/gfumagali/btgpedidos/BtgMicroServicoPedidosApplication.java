fe257aa4564ea3257652fd21a319436548c1d32f
Guilherme Fumagali Marques <guilhermefumarques@gmail.com>
2024-08-24 10:13:55 -0300

    initial commit

---
+++ src/main/java/me/gfumagali/btgpedidos/BtgMicroServicoPedidosApplication.java
@@ -0,0 +1,13 @@
+package me.gfumagali.btgpedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtgMicroServicoPedidosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtgMicroServicoPedidosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/gfumagali/btgpedidos/BtgMicroServicoPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package me.gfumagali.btgpedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtgMicroServicoPedidosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
