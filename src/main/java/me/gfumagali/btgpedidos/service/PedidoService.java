3bcb02b5126d3c5f9fc7421394e0bdfb55e1bc5c
Guilherme Fumagali Marques <guilhermefumarques@gmail.com>
2024-08-24 20:42:06 -0300

    refactor: optimizing insert with virtual threads

--- src/main/java/me/gfumagali/btgpedidos/service/PedidoService.java
+++ src/main/java/me/gfumagali/btgpedidos/service/PedidoService.java
@@ -8,9 +8,12 @@ import me.gfumagali.btgpedidos.repository.model.ClientOrders;
 import me.gfumagali.btgpedidos.repository.model.Order;
 import me.gfumagali.btgpedidos.repository.model.OrderTotalValue;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,7 +21,16 @@ public class PedidoService {
     private final ClientOrderRepository clientOrderRepository;
     private final OrderTotalValueRepository orderTotalValueRepository;
 
+    @Transactional
     public void saveOrder(OrderDto pedidoDTO) {
+        try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            executor.submit(() -> createClienteOrder(pedidoDTO));
+            executor.submit(() -> createOrderTotalValue(pedidoDTO));
+            executor.shutdown();
+        }
+    }
+
+    private void createClienteOrder(OrderDto pedidoDTO) {
         Order order = new Order(
                 String.valueOf(pedidoDTO.getCodigoPedido()),
                 LocalDateTime.now(),
@@ -36,11 +48,12 @@ public class PedidoService {
         clientOrders.setQuantidadePedidos(clientOrders.getQuantidadePedidos() + 1);
 
         clientOrderRepository.save(clientOrders);
+    }
 
+    private void createOrderTotalValue(OrderDto pedidoDTO) {
         orderTotalValueRepository.save(new OrderTotalValue(
                 pedidoDTO.getCodigoPedido(),
                 pedidoDTO.getItems().stream().mapToDouble(item -> item.getPreco() * item.getQuantidade()).sum()
         ));
     }
-
 }
