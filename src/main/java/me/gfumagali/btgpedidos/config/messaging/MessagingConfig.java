0b713a678b51088e23870532dd23ceb1960b85a4
Guilherme Fumagali Marques <guilhermefumarques@gmail.com>
2024-08-25 19:32:26 -0300

    refactor: renaming messaging configuration class

--- src/main/java/me/gfumagali/btgpedidos/config/messaging/MessagingConfig.java
+++ src/main/java/me/gfumagali/btgpedidos/config/messaging/MessagingConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class MessagingRabbitMqConfig {
+public class MessagingConfig {
     @Value("${application.listener.queue.name}")
     private String queueName;
 
