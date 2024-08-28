ce829e257e3b99975bdaf657d37b3ba3f3e357ff
Guilherme Fumagali Marques <guilhermefumarques@gmail.com>
2024-08-27 21:51:55 -0300

    fix: solving deprecated warning of RabbitListenerErrorHandler constructor

---
+++ src/main/java/me/gfumagali/btgpedidos/listener/ListenerErrorHandler.java
@@ -0,0 +1,35 @@
+package me.gfumagali.btgpedidos.listener;
+
+import com.rabbitmq.client.Channel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.listener.api.RabbitListenerErrorHandler;
+import org.springframework.amqp.rabbit.support.ListenerExecutionFailedException;
+import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class ListenerErrorHandler implements RabbitListenerErrorHandler {
+
+    @Override
+    public Object handleError(Message amqpMessage, org.springframework.messaging.Message<?> message, ListenerExecutionFailedException exception) throws Exception {
+        logError(amqpMessage, message, exception);
+        return null;
+    }
+
+    @Override
+    public Object handleError(Message amqpMessage, Channel channel, org.springframework.messaging.Message<?> message, ListenerExecutionFailedException exception) throws Exception {
+        logError(amqpMessage, message, exception);
+        return RabbitListenerErrorHandler.super.handleError(amqpMessage, channel, message, exception);
+    }
+
+    private void logError(Message amqpMessage, org.springframework.messaging.Message<?> message, ListenerExecutionFailedException exception) {
+        log.error("Error processing message {} with {} bytes", message, amqpMessage.getBody().length);
+
+        if (exception.getCause() instanceof MethodArgumentNotValidException)
+            log.error("Caused by validation error: {}", exception.getCause().getMessage());
+        else
+            log.error("Caused by: ", exception.getCause());
+    }
+}
