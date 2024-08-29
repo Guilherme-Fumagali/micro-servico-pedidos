81ddffaf81d1b6229a7c39a0f25b637507efaa9b
Guilherme Fumagali Marques <guilhermefumarques@gmail.com>
2024-08-29 15:49:05 -0300

    feat: optimizing searches by orderCode with index

--- src/main/java/me/gfumagali/btgpedidos/config/mongo/MongoConfig.java
+++ src/main/java/me/gfumagali/btgpedidos/config/mongo/MongoConfig.java
@@ -5,19 +5,29 @@ import com.mongodb.client.MongoClients;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.gfumagali.btgpedidos.config.properties.MongoProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
 @Configuration
 @Slf4j
 @RequiredArgsConstructor
-public class MongoConfig {
+public class MongoConfig extends AbstractMongoClientConfiguration {
     private final MongoProperties mongoProperties;
 
-    @Bean
+    @Override
+    public boolean autoIndexCreation() {
+        return true;
+    }
+
+    @Override
     public MongoClient mongoClient() {
         log.info("Creating mongo client with host: {}, port: {}, username: {}", mongoProperties.getHost(), mongoProperties.getPort(), mongoProperties.getUsername());
         return MongoClients.create("mongodb://" + mongoProperties.getUsername() + ":" + mongoProperties.getPassword() + "@" + mongoProperties.getHost() + ":" + mongoProperties.getPort());
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return mongoProperties.getDatabase();
     }
 
 }
