e687bb4da7812a4be0a447439ce49c48039346b2
Guilherme Fumagali Marques <guilhermefumarques@gmail.com>
2024-08-29 17:04:45 -0300

    fix: updating tests

--- src/main/java/me/gfumagali/btgpedidos/BtgMicroServicoPedidosApplication.java
+++ src/main/java/me/gfumagali/btgpedidos/BtgMicroServicoPedidosApplication.java
@@ -5,6 +5,9 @@ import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
 @OpenAPIDefinition(info =
@@ -13,8 +16,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
         version = "1.0",
         description = "API REST para exibição de dados de pedidos de clientes consumidos de uma fila",
         contact = @Contact(name = "Guilherme Fumagali", email = "guilhermefumarques@gmail.com")
-)
-)
+))
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class BtgMicroServicoPedidosApplication {
 
     public static void main(String[] args) {
